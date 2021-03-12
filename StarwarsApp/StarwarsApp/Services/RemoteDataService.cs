4a56c2c4df7940e3c1b967161e626762a2366ace
Priit Matttus <priit.mattus@tptlive.ee>
2021-03-12 15:03:57 +0200

    Starwars app

---
+++ StarwarsApp/StarwarsApp/Services/RemoteDataService.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using StarwarsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarwarsApp.Services
+{
+    public class RemoteDataService
+    {
+        public async Task<People> GetStarwarsPeople()
+        {
+            var client = new HttpClient();
+            var response = await client.GetStringAsync("https://swapi.dev/api/people/");
+            People data = null;
+            if(response != null)
+            {
+                data = JsonConvert.DeserializeObject<People>(response);
+            }
+            return data;         
+        }
+    }
+}
