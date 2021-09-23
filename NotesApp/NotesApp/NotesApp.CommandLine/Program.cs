f479258b0fb91319598646469253000f631c00e8
Priit Matttus <priit.mattus@tptlive.ee>
2021-09-23 17:00:47 +0300

    Added NotesApp

---
+++ NotesApp/NotesApp/NotesApp.CommandLine/Program.cs
@@ -0,0 +1,47 @@
+﻿using NotesApp.CommandLine.Models;
+using System;
+
+namespace NotesApp.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var run = true;
+            while (run)
+            {
+                Console.WriteLine(  
+                 @"1.Vaata märkmeid
+                2.Lisa
+                3.Muuda
+                4.Kustuta
+                5.Exit
+                            ");
+                var answer = int.Parse(Console.ReadLine());
+
+                switch (answer)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("headaega");
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine();
+                        break;
+                }
+            }            
+        }
+    }
+}
---
+++ NotesApp/NotesApp/NotesApp.Console/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotesApp.Console.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Heading { get; set; }
+        public string Content { get; set; }
+        public DateTime ChangeDateTime { get; set; }
+    }
+}
